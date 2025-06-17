3058fb7c3b113c7825bfbf82ce9600ee57575381
Nouman Khan <nknoume@gmail.com>
2025-06-17 11:13:42 +0500

    Initial backend commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const app = require('./app'); // or wherever your app.js is
-const PORT = process.env.PORT || 5000;
+const app = require('./app');
+const PORT = process.env.PORT || 3001;
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
